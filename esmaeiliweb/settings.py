703052f028a2a12f6ebc64c24d40044a1557ae67
abbas4007 <abi.esmaeili@gmail.com>
2025-09-11 17:16:29 +0330

    static

--- esmaeiliweb/settings.py
+++ esmaeiliweb/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / "static"]  # جایی که سورس فایل‌ها هستن
+STATIC_ROOT = BASE_DIR / "staticfiles"    # جایی که collectstatic خروجی رو میریزه
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
