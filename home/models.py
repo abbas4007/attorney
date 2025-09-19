f0a936030646e8cfad847522b42b6df622e3c544
abbas4007 <abi.esmaeili@gmail.com>
2025-09-19 12:05:38 +0330

    detail

--- home/models.py
+++ home/models.py
@@ -79,11 +79,12 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
-    @property
-    def description_text(self) :
-        # فقط متن خالی از تگ HTML
-        from django.utils.html import strip_tags
-        return strip_tags(self.description.html)
+    # @property
+    # def description_text(self) :
+    #     # فقط متن خالی از تگ HTML
+    #     from django.utils.html import strip_tags
+    #     return strip_tags(self.description)
+
 
     def save(self, *args, **kwargs):
         if not self.slug:  # فقط وقتی slug وارد نشده
